67871c436767bcf1a3abb416fd3641c3590167c7
Linh Duong <200393657@student.georgianc.on.ca>
2020-10-30 13:44:28 -0400

    Commit all the files finshing on Oct 30, 2020

---
+++ app/src/main/java/com/example/restorater/RestaurantActivity.kt
@@ -0,0 +1,100 @@
+package com.example.restorater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_restaurant.*
+import kotlinx.android.synthetic.main.item_comment.*
+import kotlinx.android.synthetic.main.item_comment.view.*
+
+class RestaurantActivity : AppCompatActivity() {
+    var db = FirebaseFirestore.getInstance().collection("comments")
+
+    //adaptor class tp store and bind data
+    private var adapter: RestaurantActivity.CommentsAdapter? = null
+    //create innner classes for adapter and vi=ewholder
+    private inner class CommentsViewHolder internal constructor(private val view: View) :
+    RecyclerView.ViewHolder(view) {
+
+    }
+
+    private inner class CommentsAdapter internal constructor(options: FirestoreRecyclerOptions<Comment>) :
+    FirestoreRecyclerAdapter<Comment, CommentsViewHolder>(options) {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentsViewHolder {
+            //connect the item_comment.xml template to the recyclerview
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
+            //return the ViewHolder that is now bound to item_comment.xml into the RecyclerView
+            return CommentsViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: CommentsViewHolder, position: Int, model: Comment) {
+            //assign values to the viewHolder elements
+            //itemView refers to current item in the viewHolder that is being bound to 1 object in the adapter
+
+            holder.itemView.usernameTextView.text = model.username
+            holder.itemView.bodyTextView.text = model.body
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant)
+
+        // populate Heading textview w/restaurant name
+        restaurantNameTextView.text = intent.getStringExtra("name")?.toString()
+
+        saveCommentButton.setOnClickListener {
+            // create new comment and save to firebase
+            if ((!TextUtils.isEmpty(usernameEditText.text)) && (!TextUtils.isEmpty(bodyEditText.text))) {
+                // get new id from Firebase
+                val id = db.document().id
+                val comment = Comment(id, usernameEditText.text.toString(), bodyEditText.text.toString(), intent.getStringExtra("restaurantId").toString())
+                db.document(id).set(comment)
+
+                // clear values
+                usernameEditText.setText("")
+                bodyEditText.setText("")
+                Toast.makeText(this, "Comment added", Toast.LENGTH_LONG).show()
+            }
+            else {
+                Toast.makeText(this, "Incomplete", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        //section for querying Firestore for existing comments for the selected restuanrant
+        //set RecyclerView to use LinearLayout
+        commentsRecyclerView.layoutManager = LinearLayoutManager(this)
+        //query Firestore
+        val query = db.whereEqualTo("restaurantId", intent.getStringExtra("restaurantId"))
+
+        //pass the query results to adapter for display
+        val options = FirestoreRecyclerOptions.Builder<Comment>().setQuery(query, Comment::class.java).build()
+        adapter = CommentsAdapter(options)
+
+        //bind the Comments adapter to the RecyclerView
+        commentsRecyclerView.adapter = adapter
+
+
+        }
+        //listener for changes to the underlying data
+         override fun onStart() {
+            super.onStart()
+            adapter!!.startListening()
+    }
+    // stop listening when activity is stopped (as another activity will be visible over top)
+    override fun onStop() {
+        super.onStop()
+        adapter!!.stopListening()
+    }
+}
